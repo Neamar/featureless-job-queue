6028fd2c62570b3629197c483db16f66c2aadf08
Neamar <neamar@neamar.fr>
2017-01-22 12:06:22 +0000

    Create function for single jobs

--- test/index.js
+++ test/index.js
@@ -2,7 +2,9 @@
 
 var assert = require("assert");
 var async = require("async");
+var redis = require("redis");
 
+var client = redis.createClient();
 var FJQ = require('../lib');
 
 
@@ -27,6 +29,9 @@ describe("Featureless job queue", function() {
 
   describe(".create(job, cb)", function() {
     var fjq = new FJQ();
+    beforeEach(function(done) {
+      client.del(fjq.options.redisKey, done);
+    });
 
     it("should require a job", function(done) {
       fjq.create(null, function(err) {
@@ -42,13 +47,19 @@ describe("Featureless job queue", function() {
       });
     });
 
-    it.skip("should save the job to Redis", function(done) {
+    it("should save the job to Redis", function(done) {
+      var fakeJob = {foo: "bar"};
       async.waterfall([
         function createJob(cb) {
-          fjq.create({foo: "bar"}, cb);
+          fjq.create(fakeJob, cb);
         },
-        function checkJobInRedis(cb) {
-          // ...
+        function loadJobInRedis(cb) {
+          client.lrange(fjq.options.redisKey, 0, 10, cb);
+        },
+        function checkCorrect(result, cb) {
+          assert.equal(result.length, 1);
+          assert.equal(result[0], JSON.stringify(fakeJob));
+          cb();
         }
       ], done);
     });
